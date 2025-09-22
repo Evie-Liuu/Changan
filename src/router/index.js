b0b270ea1c70d3c0c3c77337cac2c86f684bdcfd
Evie <cone306915@gmail.com>
2025-09-22 14:18:49 +0800

    故事牆模板

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,8 @@ const routes = [
   { path: "/about", name: "About", component: () => import("@/views/About.vue") },
   { path: "/actions", name: "Actions", component: () => import("@/views/Actions.vue") },
   { path: "/sdgs", name: "SDGs", component: () => import("@/views/SDGs.vue") },
-  //   { path: "/story", name: "Story", component: () => import("@/views/Story.vue") },
-  //   {
+  { path: "/story", name: "Story", component: () => import("@/views/Story.vue") },
+  // {
   //     path: "/story/:id",
   //     name: "story-detail",
   //     component: () => import("@/views/StoryDetail.vue"),
