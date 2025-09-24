53fff5832e2b4d514cb200f0be4b128475526fa3
Evie <cone306915@gmail.com>
2025-09-24 17:14:37 +0800

    素材更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <main
-    class="h-screen w-screeen relative flex flex-col justify-center items-center lg:flex-none bg-[url('@/assets/images/Web_v2_N_Bcakground.webp')] bg-cover bg-center overflow-hidden lg:bg-[url('@/assets/images/Backgound_v2_N_Hemei.webp')]"
+    class="h-screen w-screeen relative flex flex-col justify-center items-center lg:flex-none bg-[url('@/assets/images/Mobile/Mobile_Background.png')] bg-cover bg-center overflow-hidden lg:bg-[url('@/assets/images/Web_Background.png')]"
   >
     <!-- School Title -->
-    <section class="lg:absolute lg:top-8 lg:left-8">
-      <div>彰化縣</div>
-      <div>長安</div>
-      <div>國小</div>
+    <section class="lg:absolute lg:-top-2 lg:right-10">
+      <img
+        src="@/assets/images/ChangAn_Logo.png"
+        class="hidden lg:block pointer-events-none select-none w-35"
+        alt="Changan_logo"
+      />
     </section>
+
     <!-- Cover -->
     <section class="">
       <div
@@ -20,17 +23,35 @@
         <img
           :src="IceCreamMelting"
           class="pointer-events-none select-none"
-          alt="Hemei_cover"
+          alt="Changan_cover"
         />
         <!-- <img
           :src="IceCreamMelting_mobile"
           class="block lg:hidden pointer-events-none select-none"
-          alt="Hemei_cover"
+          alt="Changan_cover"
         /> -->
       </div>
     </section>
+    <!-- School Title -->
+    <section class="">
+      <div
+        :class="[
+          'transition-all duration-3000 ease-out',
+          isLoaded ? 'opacity-100' : 'opacity-0',
+        ]"
+        class="lg:w-220 lg:h-full flex items-center justify-center lg:justify-start"
+      >
+        <img
+          src="@/assets/images/ChangAn_School_title.png"
+          class="pointer-events-none select-none"
+          alt="Changan_cover"
+        />
+      </div>
+    </section>
     <!-- Menu Buttons -->
-    <section class="h-full flex items-center lg:items-end justify-center">
+    <section
+      class="h-full flex items-center lg:items-end justify-center font-bold text-green-dark"
+    >
       <div
         :class="[
           'transition-all duration-3000 ease-out',
@@ -45,12 +66,12 @@
           @mouseleave="hoveredIndex = null"
         >
           <img
-            :src="hoveredIndex === 0 ? button1Selected : button1Unselected"
-            class="pointer-events-none select-none w-52 lg:w-auto"
+            :src="hoveredIndex === 0 ? button1Selected : buttonUnselected"
+            class="pointer-events-none select-none w-52 lg:w-60"
             alt="Button_1"
           />
           <span
-            class="absolute inset-0 flex items-center justify-center text-white ps-5 tracking-widest"
+            class="absolute inset-0 flex items-center justify-center ps-5 tracking-widest"
             >校園探索</span
           >
         </router-link>
@@ -60,12 +81,12 @@
           @mouseover="hoveredIndex = 1"
           @mouseleave="hoveredIndex = null"
           ><img
-            :src="hoveredIndex === 1 ? button2Selected : button2Unselected"
-            class="pointer-events-none select-none w-52 lg:w-auto"
+            :src="hoveredIndex === 1 ? button2Selected : buttonUnselected"
+            class="pointer-events-none select-none w-52 lg:w-60"
             alt="Button_2"
           />
           <span
-            class="absolute inset-0 flex items-center justify-center text-white ps-5 tracking-widest"
+            class="absolute inset-0 flex items-center justify-center ps-5 tracking-widest"
             >SDGs行動</span
           ></router-link
         >
@@ -75,12 +96,12 @@
           @mouseover="hoveredIndex = 2"
           @mouseleave="hoveredIndex = null"
           ><img
-            :src="hoveredIndex === 2 ? button3Selected : button3Unselected"
-            class="pointer-events-none select-none w-52 lg:w-auto"
+            :src="hoveredIndex === 2 ? button3Selected : buttonUnselected"
+            class="pointer-events-none select-none w-52 lg:w-60"
             alt="Button_3"
           />
           <span
-            class="absolute inset-0 flex items-center justify-center text-white ps-5 tracking-widest"
+            class="absolute inset-0 flex items-center justify-center ps-5 tracking-widest"
             >SDGs成果</span
           ></router-link
         >
@@ -90,13 +111,11 @@
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
-import IceCreamMelting from "@/assets/images/ice_cream_community.png";
-import button1Unselected from "@/assets/images/Paint_1_v2_Unselected.png";
-import button1Selected from "@/assets/images/Paint_1_v2_Selected.png";
-import button2Unselected from "@/assets/images/Paint_2_v2_Unselected.png";
-import button2Selected from "@/assets/images/Paint_2_v2_Selected.png";
-import button3Unselected from "@/assets/images/Paint_3_v2_Unselected.png";
-import button3Selected from "@/assets/images/Paint_3_v2_Selected.png";
+import IceCreamMelting from "@/assets/images/Earth_IceCream.webp";
+import buttonUnselected from "@/assets/images/Bar_Unselected.png";
+import button1Selected from "@/assets/images/Bar_Selected_School.png";
+import button2Selected from "@/assets/images/Bar_Selected_Action.png";
+import button3Selected from "@/assets/images/Bar_Selected_Result.png";
 
 const hoveredIndex = ref(null);
 const isLoaded = ref(false);
