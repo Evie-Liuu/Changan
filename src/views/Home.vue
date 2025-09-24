bde8cce7d3bea57417abe4a3621c498231639218
Evie <cone306915@gmail.com>
2025-09-24 18:10:28 +0800

    素材更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <main
-    class="h-screen w-screeen relative flex flex-col justify-center items-center lg:flex-none bg-[url('@/assets/images/Mobile/Mobile_Background.png')] bg-cover bg-center overflow-hidden lg:bg-[url('@/assets/images/Web_Background.png')]"
+    :class="[
+      'h-screen w-screeen relative flex flex-col justify-center items-center gap-5 bg-[url(\'@/assets/images/Mobile/Mobile_Background.png\')] bg-cover bg-center overflow-hidden lg:bg-[url(\'@/assets/images/Web_Background.png\')]',
+      'transition-all duration-2000 ease-out',
+      shouldMoveUp ? '-translate-y-8 lg:-translate-y-16' : 'translate-y-0'
+    ]"
   >
     <!-- School Title -->
     <section class="lg:absolute lg:-top-2 lg:right-10">
@@ -18,7 +22,7 @@
           'transition-all duration-3000 ease-out',
           isLoaded ? 'opacity-100' : 'opacity-0',
         ]"
-        class="lg:w-220 lg:h-full flex items-center justify-center lg:justify-start"
+        class="flex items-center justify-center lg:max-w-320"
       >
         <img
           :src="IceCreamMelting"
@@ -39,7 +43,7 @@
           'transition-all duration-3000 ease-out',
           isLoaded ? 'opacity-100' : 'opacity-0',
         ]"
-        class="lg:w-220 lg:h-full flex items-center justify-center lg:justify-start"
+        class="lg:w-180 flex items-center justify-center lg:justify-start"
       >
         <img
           src="@/assets/images/ChangAn_School_title.png"
@@ -50,14 +54,14 @@
     </section>
     <!-- Menu Buttons -->
     <section
-      class="h-full flex items-center lg:items-end justify-center font-bold text-green-dark"
+      class="h-full flex items-center justify-center font-bold text-green-dark"
     >
       <div
         :class="[
           'transition-all duration-3000 ease-out',
           isLoaded ? 'opacity-100' : 'opacity-0',
         ]"
-        class="flex flex-col lg:flex-row lg:gap-5 items-center justify-center text-xl lg:text-2xl lg:pb-25"
+        class="flex flex-col lg:flex-row lg:gap-20 items-center justify-center text-xl lg:text-2xl lg:pb-25"
       >
         <router-link
           to="/about"
@@ -71,7 +75,7 @@
             alt="Button_1"
           />
           <span
-            class="absolute inset-0 flex items-center justify-center ps-5 tracking-widest"
+            class="absolute inset-0 flex items-center justify-center tracking-widest -translate-y-4"
             >校園探索</span
           >
         </router-link>
@@ -86,13 +90,13 @@
             alt="Button_2"
           />
           <span
-            class="absolute inset-0 flex items-center justify-center ps-5 tracking-widest"
+            class="absolute inset-0 flex items-center justify-center tracking-widest -translate-y-4"
             >SDGs行動</span
           ></router-link
         >
         <router-link
           to="/sdgs"
-          class="relative -translate-x-1/6"
+          class="relative"
           @mouseover="hoveredIndex = 2"
           @mouseleave="hoveredIndex = null"
           ><img
@@ -101,7 +105,7 @@
             alt="Button_3"
           />
           <span
-            class="absolute inset-0 flex items-center justify-center ps-5 tracking-widest"
+            class="absolute inset-0 flex items-center justify-center tracking-widest -translate-y-4"
             >SDGs成果</span
           ></router-link
         >
@@ -119,10 +123,15 @@ import button3Selected from "@/assets/images/Bar_Selected_Result.png";
 
 const hoveredIndex = ref(null);
 const isLoaded = ref(false);
+const shouldMoveUp = ref(false);
 
 onMounted(() => {
   setTimeout(() => {
     isLoaded.value = true;
+    // 延遲一點時間後開始向上移動動畫
+    setTimeout(() => {
+      shouldMoveUp.value = true;
+    }, 500);
   }, 100);
 });
 </script>
